900fd5071acf512781cc275fb46740c35ac541c8
Victoria <vic0307@mail.ru>
2019-04-12 12:50:29 +0300

    Frontend added

--- tms-ui/src/main/frontend/src/index.js
+++ tms-ui/src/main/frontend/src/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/component/App";
-import "./index.css";
+//import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
+import TaskInfo from "./app/component/Task/TaskInfo";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render((
+        <Router>
+            <div>
+
+
+                {/*<Route path="/tasks" component={TaskInfo}/>*/}
+                <Route path="/tasks/:id" component={TaskInfo}/>
+                <Route path="/App" component={App}/>
+
+            </div>
+        </Router>),
+    document.getElementById("root")
+);
